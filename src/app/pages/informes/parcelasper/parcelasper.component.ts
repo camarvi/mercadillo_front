95405655283536fd392e29ee3478b8846ecec883
camarvi <carlosj.marfil@gmail.com>
2022-02-01 13:54:42 +0100

    Listado Personas parcelas

--- src/app/pages/informes/parcelasper/parcelasper.component.ts
+++ src/app/pages/informes/parcelasper/parcelasper.component.ts
@@ -15,16 +15,23 @@ export class ParcelasperComponent implements OnInit {
 
   public puestospersonas : PersonasPuestoInterface[] = [];
   public noencontrados : boolean = false;
+  public cargando : boolean = false;
 
   constructor(private mercadillosService : MercadillosService) { }
 
   ngOnInit(): void {
-
+      
+    this.cargando = true;
        this.mercadillosService.getInformePersonasParcelas()
            .subscribe( (resp:PersonasPuestoInterface[])=>{
              this.puestospersonas = resp;
              console.log(this.puestospersonas);
-             this.noencontrados = true;
+             if (this.puestospersonas.length<1){
+              this.noencontrados = true;
+            } else {
+              this.noencontrados = false;
+            }
+            this.cargando = false;
            }) 
     
   }
